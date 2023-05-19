4182a2d1e412e10cc38081fb02f586f810d99478
tanvir-hossen49 <thossen49@gmail.com>
2023-05-19 17:45:11 +0600

    update user and password of mongodb

--- index.js
+++ index.js
@@ -31,16 +31,19 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
-  } catch (error) {
-    console.log(error);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
   }
 }
-run();
+run().catch(console.dir);
 
 app.listen(PORT, () => {
   console.log("app is running on port ", PORT);
